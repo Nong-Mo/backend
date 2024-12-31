6fe2e1e640830c854b143891016b9868e0095e05
DongHyunKim <kimtoy821@gmail.com>
2025-01-01 02:13:30 +0900

    [Chore] Pydantic v2  설정 변경

--- app/models/image.py
+++ app/models/image.py
@@ -35,6 +35,6 @@ class ImageDocument(BaseModel):
     created_at: str
 
     class Config:
-        allow_population_by_field_name = True
+        populate_by_name = True
         arbitrary_types_allowed = True
         json_encoders = {ObjectId: str}
