e6a899b2b404dc3bf8ec70e9c3d2852256f16112
DongHyunKim <kimtoy821@gmail.com>
2025-01-01 21:51:12 +0900

    [Chore] 디버깅 임포트

--- app/routes/image.py
+++ app/routes/image.py
@@ -3,6 +3,7 @@ from app.schemas.image import ImageUploadRequest, ImageUploadResponse
 from app.services.image_services import ImageService
 from typing import List
 from app.services.image_services import verify_jwt
+import pdb
 
 router = APIRouter()
 image_service = ImageService()
@@ -16,6 +17,7 @@ async def upload_images(
     """
     이미지를 업로드하고 OCR을 수행하여 결과를 반환
     """
+    # pdb.set_trace()
     result = await image_service.process_images(title=title, files=files, user_id=user_id)
     return ImageUploadResponse(
         file_id=result.file_id,
