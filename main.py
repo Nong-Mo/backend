62a6b9a24317fe3e8cb3530851ae34df12413758
seoha <a1vm5h@gmail.com>
2025-01-13 04:44:50 +0900

    [Fix] 스웨거 관련 설정 추가

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 from fastapi import FastAPI
 from app.routes import auth, image, storage, llm
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.openapi.utils import get_openapi
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(title="AtoD")
 
+origins = [
+    "http://localhost:3000",
+    "https://e336-175-195-226-193.ngrok-free.app",
+    "http://ec2-54-180-149-98.ap-northeast-2.compute.amazonaws.com",
+    "https://nongmo-a2d.com",
+    "*"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://e336-175-195-226-193.ngrok-free.app",
-        "http://ec2-54-180-149-98.ap-northeast-2.compute.amazonaws.com",
-        "https://nongmo-a2d.com",
-        "*"
-    ],
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],  # 모든 HTTP 메서드 허용 (POST, GET, OPTIONS 등)
-    allow_headers=["*"],  # 모든 HTTP 헤더 허용
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/health")
@@ -33,3 +37,26 @@ app.include_router(storage.router, prefix="/storage", tags=["storage"])
 
 # llm 관련 라우트 등록
 app.include_router(llm.router, prefix="/llm", tags=["llm"])
+
+# Swagger 설정 추가
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="AtoD API",  # 스웨거 문서 제목
+        version="1.0.0",
+        description="AtoD API 문서입니다.", # 스웨거 문서 설명
+        routes=app.routes,
+    )
+    openapi_schema["components"]["securitySchemes"] = {
+        "bearerAuth": {
+            "type": "http",
+            "scheme": "bearer",
+            "bearerFormat": "JWT",
+        }
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+app.openapi = custom_openapi
+app.mount("/docs", StaticFiles(directory="static"), name="static")
